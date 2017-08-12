9617d3889681dc645a12ff43a77dab516ecc7480
sgr13 <sgr13@student.edu>
2017-08-12 08:58:19 +0200

    ready admin/user panel

--- src/PatientBundle/Controller/RegisterController.php
+++ src/PatientBundle/Controller/RegisterController.php
@@ -191,7 +191,8 @@ class RegisterController extends Controller
             ));
 
         } else {
-            throw new ValidatorException("Podano niewłaściwy kod.");
+            return $this->render('PatientBundle:Register:visitSummary.html.twig', array(
+            ));
         }
     }
 
